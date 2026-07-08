0be5c9d3de954b50018fe990d91a44afce7cd8ef
dharshinirug24cs-dev <dharshinir.ug.24.cs@scadengineering.ac.in>
2026-07-08 23:15:55 +0530

    Fixed ALLOWED_HOSTS

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-k%(x=scqh9p!vrm!&9%=v)#6yxg+t0*k!bknuy$ynz$+1w*$o9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "restaurant-management-system-ho2h.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
